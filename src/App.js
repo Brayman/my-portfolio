04f17657f7ebe13a01e48f825bff9ebbddac5ac1
Artem <Ar.rakitskiy@gmail.com>
2022-05-26 17:27:08 +0400

    добавил роутинг

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './components/common.css';
-import SignUp from './components/SignUp/Signup';
+import './App.css';
+import SignUp from './components/Sign/Signup';
 import Card from './components/Card/Card';
+import List from './components/List/List';
+import Signin from './components/Sign/Signin';
+import { BrowserRouter, Route, NavLink, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <section>
-      <Card/>
-    </section>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/list' element={<List />} />
+        <Route path='/card' element={<Card />} />
+      </Routes>
+      <footer className='app__footer'>
+        <NavLink to='signin' className='footer__link'>link</NavLink>
+        <NavLink to='signup' className='footer__link'>link</NavLink>
+        <NavLink to='list' className='footer__link'>link</NavLink>
+        <NavLink to='card' className='footer__link'>link</NavLink>
+      </footer>
+
+
+    </BrowserRouter>
   );
 }
 
